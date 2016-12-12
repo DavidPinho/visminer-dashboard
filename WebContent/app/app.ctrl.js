34037ecf8a1d11277fff253cf93e838d1820e974
Sandro Campos <contato@sandrocampos.com>
2016-12-12 17:24:05 -0300

    + toastr support

--- WebContent/app/app.ctrl.js
+++ WebContent/app/app.ctrl.js
@@ -237,3 +237,21 @@ homeApp.factory('TDItem', function(Commit, Committer) {
 	};
 	return TDItem;
 })
+
+toastr.options = {
+  "closeButton": false,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-bottom-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
