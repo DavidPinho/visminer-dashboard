62a2d394aab396b272148190b3b91af0460b10de
Sandro Campos <contato@sandrocampos.com>
2017-02-11 01:20:07 -0300

    fixed the json format

--- src/org/visminer/controller/TDManagementController.java
+++ src/org/visminer/controller/TDManagementController.java
@@ -9,7 +9,12 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.bson.Document;
+import org.repositoryminer.model.Reference;
+import org.repositoryminer.persistence.handler.ReferenceDocumentHandler;
+import org.repositoryminer.scm.ReferenceType;
 import org.repositoryminer.technicaldebt.persistence.TechnicalCodeDebtDocumentHandler;
+
+import com.mongodb.client.model.Projections;
 
 @Path("td-management")
 public class TDManagementController {
@@ -17,18 +22,19 @@ public class TDManagementController {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("find")
-	public String find(@QueryParam("commit") final String commit, @QueryParam("types") final List<String> types,
+	public String find(@QueryParam("repositoryId") final String repositoryId, @QueryParam("tag") final String tag, @QueryParam("types") final List<String> types,
 			@QueryParam("indicators") final List<String> indicators, @QueryParam("is_td") final Boolean isTd) {
+		ReferenceDocumentHandler refHandler = new ReferenceDocumentHandler();
+		Reference r = Reference.parseDocument(refHandler.findByNameAndType(tag, ReferenceType.TAG, repositoryId, Projections.slice("commit", 1)));
 		TechnicalCodeDebtDocumentHandler handler = new TechnicalCodeDebtDocumentHandler();
-		List<Document> docs = handler.findToManagement(commit, types, indicators, isTd);
-
-		StringBuilder builder = new StringBuilder("{");
+		List<Document> docs = handler.findToManagement(r.getCommits().get(0), types, indicators, isTd);
+		System.out.println(docs);
+		String json = "[";
 		for (Document d : docs) {
-			builder.append(d.toJson()).append(",");
+			json += d.toJson()+",";
 		}
-		builder.append("}");
-
-		return builder.toString();
+		json = json.substring(0, json.length()-1) + "]";
+		return json;
 	}
 
 }
