c9eae6d0584979077d5a98bc552ce26064d81074
Sandro Campos <contato@sandrocampos.com>
2017-02-11 01:18:12 -0300

    + repository restiful resources

---
+++ src/org/visminer/controller/RepositoryController.java
@@ -0,0 +1,51 @@
+package org.visminer.controller;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.bson.Document;
+import org.repositoryminer.model.Repository;
+import org.repositoryminer.persistence.handler.ReferenceDocumentHandler;
+import org.repositoryminer.persistence.handler.RepositoryDocumentHandler;
+
+import com.mongodb.client.model.Projections;
+
+@Path("repository")
+public class RepositoryController {
+
+	@GET
+	@Path("get-repositories")
+	public String getRepositories() {
+		RepositoryDocumentHandler repoHandler = new RepositoryDocumentHandler();
+		List<Document> docs = repoHandler.findAll();
+		StringBuilder builder = new StringBuilder("[");
+		for (Document d : docs) {
+			builder.append(d.toJson());
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	@GET
+	@Path("get-references")
+	public String getReferences(@QueryParam("repositoryId") final String repositoryId) {
+		RepositoryDocumentHandler handler = new RepositoryDocumentHandler();
+        Repository repo = Repository.parseDocument(handler.findById(repositoryId, Projections.include("path")));
+		ReferenceDocumentHandler refHandler = new ReferenceDocumentHandler();
+		List<Document> docs = refHandler.getByRepository(repo.getId());
+		StringBuilder builder = new StringBuilder("[");
+		for (Document d : docs) {
+			builder.append(d.toJson());
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+}
