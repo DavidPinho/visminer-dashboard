4a0c6a729368b1acb02a63eb931ec4df156dd734
DavidPinho <davidpinho08@gmail.com>
2017-07-12 01:36:58 +0100

    [tdmanagement] Fix kanban dashboard

--- app/technicaldebt/tdmanagement/tdmanagement.ctrl.js
+++ app/technicaldebt/tdmanagement/tdmanagement.ctrl.js
@@ -1,15 +1,18 @@
 homeApp = angular.module('homeApp');
 
-homeApp.controller('TDManagementCtrl', function($scope, $http,
- $route, sidebarService, tdTimelineService){
+homeApp.controller('TDManagementCtrl', function ($scope, $http,
+	$route, sidebarService, tdTimelineService) {
 	var thisCtrl = this;
-	var DebtStatus = Object.freeze({UNEVALUATED: 0, TODO: 1, DOING: 2, DONE: 3});
+	var DebtStatus = Object.freeze({ UNEVALUATED: 0, TODO: 1, DOING: 2, DONE: 3 });
 
 	$scope.currentPage = sidebarService.getCurrentPage();
 	$scope.master = null;
 	$scope.types = [];
 	$scope.todoCode = [];
 	$scope.todoDesign = [];
+	$scope.todoDefect = [];
+	$scope.todoTest = [];
+	$scope.todoRequirement = [];
 	$scope.doingCode = [];
 	$scope.doingDesign = [];
 	$scope.doneCode = [];
@@ -17,92 +20,89 @@ homeApp.controller('TDManagementCtrl', function($scope, $http,
 	$scope.filtered.repository = sidebarService.getRepository();
 	$scope.filtered.debts = sidebarService.getDebts();
 
-	thisCtrl.selectView = function(view) {
+	thisCtrl.selectView = function (view) {
 		$scope.currentPage = view;
 		sidebarService.setCurrentPage(view);
 	}
 
-	thisCtrl.loadMaster = function() {
+	thisCtrl.loadMaster = function () {
 		if ($scope.filtered.repository) {
 			var repositoryId = $scope.filtered.repository._id;
-			$http.get('TreeServlet', {params:{"action": "getMaster", "repositoryId": repositoryId}})
-			.success(function(data) {
-				console.log('loading master: ', data);	 
-				$scope.master = data;
-				thisCtrl.loadTypes($scope.master._id);
-			});
-		}	
+			$http.get('../../data/rm_references.json')
+				.success(function (data) {
+					console.log('Loading master');
+					for (var i in data) {
+						var tag = data[i];
+						if (tag.repository === repositoryId && tag.path === "refs/heads/master") {
+							$scope.master = tag;
+							thisCtrl.loadTypes($scope.master.name);
+							break;
+						}
+					}
+				});
+		}
 	}
 
 	thisCtrl.loadMaster();
 
-	thisCtrl.loadTypes = function(tagId) {
-		$http.get('TypeServlet', {params:{"action": "getAllByTree", "treeId": tagId}})
-		.success(function(data) {
-			console.log('found', data.length, ' types'); 
-			for (var i = 0; i < data.length; i++) {
-				var hasDebt = thisCtrl.loadCards(data[i]);
-				if (hasDebt) {
-					$scope.types.push(data[i]);				
-				}				
-			}
-		});
-	}
-
-	thisCtrl.loadCards = function(type) {
-		var hasDebt = false;
-		if (type.abstract_types[0]) {
-			var debtsList = type.abstract_types[0].technicaldebts;
-			if (debtsList.length > 0) {
-				for (var j = 0; j < debtsList.length; j++) {
-					if (debtsList[j].name == 'Code Debt' && $.inArray('CODE', $scope.filtered.debts) > -1 && debtsList[j].value && debtsList[j].status > 0) {
-						hasDebt = true;
-						switch (debtsList[j].status) {
-							case DebtStatus.TODO: 
-								$scope.todoCode.push(type);
-								break;
-							case DebtStatus.DOING: 
-								$scope.doingCode.push(type);
-								break;
-							case DebtStatus.DONE: 
-								$scope.doneCode.push(type);
-								break;	
-						}
+	thisCtrl.loadTypes = function (tagName) {
+		$http.get('../../data/rm_technical_code_debt.json')
+			.success(function (data) {
+				for (var i = 0; i < data.length; i++) {
+					if (data[i].reference_name === tagName && data[i].debts.length > 0) {
+						thisCtrl.loadCards(data[i]);
+						$scope.types.push(data[i]);
 					}
-					if (debtsList[j].name == 'Design Debt'  && $.inArray('DESIGN', $scope.filtered.debts) > -1 && debtsList[j].value && debtsList[j].status > 0) {
-						hasDebt = true;
-						switch (debtsList[j].status) {
-							case DebtStatus.TODO: 
-								$scope.todoDesign.push(type);
-								break;
-							case DebtStatus.DOING: 
-								$scope.doingDesign.push(type);
-								break;
-							case DebtStatus.DONE: 
-								$scope.doneDesign.push(type);
-								break;	
-						}
-					}
-				}			
+				}
+			});
+	}
+
+	thisCtrl.loadCards = function (type) {
+		console.log("load cards");
+		var debtsList = type.debts;
+		if (debtsList.length > 0) {
+			for (var j = 0; j < debtsList.length; j++) {
+				if (debtsList[j] === 'CODE_DEBT') {
+					$scope.todoCode.push(type);
+				}
+				if (debtsList[j] === 'DESIGN_DEBT') {
+					$scope.todoDesign.push(type);
+				}
+				if (debtsList[j] === 'TEST_DEBT') {
+					$scope.todoTest.push(type);
+				}
+				if (debtsList[j] === 'DEFECT_DEBT') {
+					$scope.todoDefect.push(type);
+				}
+				if (debtsList[j] === 'REQUIREMENT_DEBT') {
+					$scope.todoRequirement.push(type);
+				}
 			}
-		}	
-		return hasDebt;
+		}
 	}
 
-	$scope.updateDebtStatus = function(type, debt, status) {
-		$http.get('TypeServlet', {params:{"action": "updateDebtStatus",
-		 "commitId": type.commit, "fileId": type.file_hash, "debt": debt, "status": status}})
-		.success(function() {
-			$route.reload();					
-		});
+	$scope.updateDebtStatus = function (type, debt, status) {
+		$http.get('TypeServlet', {
+			params: {
+				"action": "updateDebtStatus",
+				"commitId": type.commit, "fileId": type.file_hash, "debt": debt, "status": status
+			}
+		})
+			.success(function () {
+				$route.reload();
+			});
 	}
 
-	$scope.convertDate = function(commitDate) {
-		return moment(new Date(commitDate.$date)).format('DD/MM/YYYY');
+	$scope.convertDate = function (commitDate) {
+		return moment(new Date(commitDate.substring(0,10))).format('DD/MM/YYYY');
 	}
 
-	$scope.showTdTimeline = function(type) {
+	$scope.showTdTimeline = function (type) {
 		tdTimelineService.setType(type);
 		$('#tdTimelineModal').modal('show');
+	}
+
+	$scope.substringFileName = function (fileName) {
+		return fileName.substring(fileName.lastIndexOf("/") + 1, fileName.lastIndexOf(".java"));
 	}
 });
